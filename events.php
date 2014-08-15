918ae23bae3a3769d9ca98e05332b1835c15a90a
Shahi <i-neda@i-nedas-MacBook-Pro.local>
2014-08-15 09:11:41 +0100

    events page added

---
+++ events.php
@@ -0,0 +1,69 @@
+<?php
+$page = 'events';
+?>
+<?php include("inc/head.php") ?>
+		<section class="main members-page">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8">
+					<!-- Main content -->
+						<div class="panel panel-home">
+						 <div class="panel-body">
+						 	<h1>BCCUK Event Calendar</h1>
+						 	<div class="calendar-container2">
+						 		<iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=bccuk.co.uk%40gmail.com&amp;color=%232952A3&amp;ctz=Europe%2FLondon" style=" border-width:0 " width="720" height="600" frameborder="0" scrolling="no"></iframe>
+						 	</div>
+						 </div><!-- /panel-body -->
+						</div>
+
+						<div class="panel panel-home">
+						 <div class="panel-body">
+						 	<h1>Recent Activities</h1>
+						 	<div id="owl-video-demo" class="video-gallery">
+						 	  <div class="item">
+						 		<iframe width="640" height="360" src="//www.youtube.com/embed/YiYBal0lVl8?rel=0" frameborder="0" allowfullscreen></iframe>
+						 	  </div>
+							  <div class="item">
+						 		<iframe src="//player.vimeo.com/video/46675854?portrait=0&amp;color=ff0179" width="710" height="400" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+						 	  </div>
+
+						 	</div>
+						 </div>
+						</div>
+
+						<div class="panel panel-home">
+						 <div class="panel-body">
+						 	<h1>Photo Gallery</h1>
+						 	<!-- <iframe src="//player.vimeo.com/video/46675854?portrait=0&amp;color=ff0179" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> -->
+						 	<div id="owl-demo" class="gallery">
+							  <div class="item"><a href="http://bccuk.co.uk/buddha%20Jayanti%202014%20-%20BCCUK/album/index.html"><img src="http://bccuk.co.uk/buddha%20Jayanti%202014%20-%20BCCUK/album/slides/IMG_8791.JPG" alt="Owl Image"><span class="caption">Buddha Jayanti 2014</span><span class="bg"></span></a></div>
+							  <div class="item"><a href="http://bccuk.co.uk/Maha%20Kaal%20Puja%20-%20BCCUK/album/index.html"><img src="http://bccuk.co.uk/Maha%20Kaal%20Puja%20-%20BCCUK/album/slides/IMG_8555.JPG" alt="Owl Image"><span class="caption">Maha Kaal Puja</span><span class="bg"></span></a></div>
+							  <div class="item"><a href="http://bccuk.co.uk/Charity%20Dinner%20Night%20BCCUK%202014/album/index.html"><img src="http://bccuk.co.uk/Charity%20Dinner%20Night%20BCCUK%202014/album/slides/IMG_8107.JPG" alt="Owl Image"><span class="caption">Charity Dinner Night</span><span class="bg"></span></a></div>
+							  <div class="item"><a href="https://onedrive.live.com/?cid=6F239E296D484D6C&id=6F239E296D484D6C%2110926&authkey=%21Aqz9WUZ35Ofnw64"><img src="https://zkdwda.dm2302.livefilestore.com/y2mAw0DHsym5iuIYwxmv917rJxlXe1lHze0AGbGkXIDqxvLUPbOWgs8KoTX5NEGC1Kw6OPMoARswDNG0uABbJCr6YfSqkMoBD1YfFWPBEg0oKvt6-UdEUZiImj_BJYh02Jl/SGRG2881.jpg?psid=1" alt="Owl Image"><span class="caption">Tara Puja</span><span class="bg"></span></a></div>
+							  <div class="item"><a href="http://bccuk.co.uk/25-05-2013%20Buddha%20Jayanti/"><img src="http://bccuk.co.uk/25-05-2013%20Buddha%20Jayanti/slides/IMG_3670.JPG" alt="Owl Image"><span class="caption">Buddha Jayanti 2013</span><span class="bg"></span></a></div>
+							  <!-- <div class="item"><a href="http://bccuk.co.uk/buddha%20Jayanti%202014%20-%20BCCUK/album/index.html"><img src="http://bccuk.co.uk/buddha%20Jayanti%202014%20-%20BCCUK/album/slides/IMG_8807.JPG" alt="Owl Image"><span class="caption">Buddha Jayanti</span><span class="bg"></span></a></div>
+							  <div class="item"><a href="http://bccuk.co.uk/buddha%20Jayanti%202014%20-%20BCCUK/album/index.html"><img src="http://bccuk.co.uk/buddha%20Jayanti%202014%20-%20BCCUK/album/slides/IMG_8812.JPG" alt="Owl Image"><span class="caption">Buddha Jayanti</span><span class="bg"></span></a></div>
+							  <div class="item"><a href="http://bccuk.co.uk/buddha%20Jayanti%202014%20-%20BCCUK/album/index.html"><img src="http://bccuk.co.uk/buddha%20Jayanti%202014%20-%20BCCUK/album/slides/IMG_8817.JPG" alt="Owl Image"><span class="caption">Buddha Jayanti</span><span class="bg"></span></a></div> -->
+							</div>
+							 
+						 </div>
+						</div>
+
+
+
+		
+							
+													
+					<!-- /Main content -->
+					</div>
+					<div class="col-md-4 sidebar">
+					<!-- Sidebar -->
+						<?php include("inc/news.php") ?>
+						<?php include("inc/events.php") ?>	
+					<!-- /Sidebar -->
+					</div>
+				</div>
+				
+			</div>
+		</section>
+<?php include("inc/foot.php") ?>
